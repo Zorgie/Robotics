888b0bab6315af47f68fd59df3ffdc5b7ebf8aa0
robo <robo@NUC.(none)>
2013-11-08 14:20:11 +0100

    Made small changes to the Rotation Class   \Rui

--- movement/src/Rotation.cpp
+++ movement/src/Rotation.cpp
@@ -24,11 +24,23 @@ movement::wheel_speed Rotation::step(differential_drive::Encoders &enc){
 	movement::wheel_speed speed;
 	float right_wheel_delta = enc.delta_encoder1;
 	float left_wheel_delta = enc.delta_encoder2;
-	if(abs(degrees_turned - degrees_target) > 10){
+	/*if(abs(degrees_turned - degrees_target) > 10){
 		speed.W1 = SPEED;
 		speed.W2 = -SPEED;
+	}*/
+	if(abs(degrees_turned - degrees_target) > 20){
+		speed.W1=SPEED;
+		speed.W2=-SPEED;
 	}
+	else{
+		if(abs(degrees_turned - degrees_target) <= 20){ // Smoothen the braking
+			speed.W1=SPEED*(abs((degrees_turned - degrees_target)/20.0));
+			speed.W2=-SPEED*(abs((degrees_turned - degrees_target)/20.0));
+		}
+		else{
+			prinft("Unexpected behaviour \n");
+		}
+	}
+
 	return speed;
 }
-
-
