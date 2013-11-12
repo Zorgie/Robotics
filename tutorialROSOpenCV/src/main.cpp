3e6a792ebb710fa82bf8397c8fbcbea486f31032
robo <robo@robo-VirtualBox.(none)>
2013-11-12 17:38:16 +0100

    Can now identify several vegetables with unheard accuracy and computation speed.

--- tutorialROSOpenCV/src/main.cpp
+++ tutorialROSOpenCV/src/main.cpp
@@ -7,119 +7,104 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <stdio.h>
-
+#include <cmath>
 
 namespace enc = sensor_msgs::image_encodings;
 
 static const char WINDOW[] = "Original Window";
 static const char WINDOW2[] = "Process Window";
 
-cv::Mat findRed(cv::Mat imgHSV){       
-       cv::Mat imgThresh=cvCreateImage(cvSize(640,480),IPL_DEPTH_8U, 1);
-       inRange(imgHSV, cv::Scalar(0,0,128), cv::Scalar(70,90,255), imgThresh);
-       return imgThresh;
-} 
-
-cv::Mat findYellow(cv::Mat imgHSV){       
-       cv::Mat imgThresh=cvCreateImage(cvSize(640,480),IPL_DEPTH_8U, 1);
-       inRange(imgHSV, cv::Scalar(0,180,154), cv::Scalar(90,255,255), imgThresh);
-       return imgThresh;
+cv::Mat findRed(cv::Mat imgHSV) {
+	cv::Mat imgThresh = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
+	inRange(imgHSV, cv::Scalar(0, 0, 128), cv::Scalar(70, 90, 255), imgThresh);
+	return imgThresh;
 }
 
-class ImageConverter
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  
+cv::Mat findYellow(cv::Mat imgHSV) {
+	cv::Mat imgThresh = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
+	inRange(imgHSV, cv::Scalar(0, 180, 154), cv::Scalar(90, 255, 255),
+			imgThresh);
+	return imgThresh;
+}
+
+class ImageConverter {
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;
+
 public:
-  ImageConverter()
-    : it_(nh_)
-  {
-    image_pub_ = it_.advertise("out", 1);
-    image_sub_ = it_.subscribe("/camera/rgb/image_color", 1, &ImageConverter::imageCb, this);
+	ImageConverter() :
+			it_(nh_) {
+		image_pub_ = it_.advertise("out", 1);
+		image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
+				&ImageConverter::imageCb, this);
 
-    cv::namedWindow(WINDOW);
-    cv::namedWindow(WINDOW2);
-  }
+		cv::namedWindow(WINDOW);
+		cv::namedWindow(WINDOW2);
+	}
 
-  ~ImageConverter()
-  {
-    cv::destroyWindow(WINDOW);
-    cv::destroyWindow(WINDOW2);
-  }
+	~ImageConverter() {
+		cv::destroyWindow(WINDOW);
+		cv::destroyWindow(WINDOW2);
+	}
 
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
+	int count(cv::Mat image){
+		int count = 0;
+		for (int x = 0; x < image.rows; x++) {
+			for (int y = 0; y < image.cols; y++) {
+				int k = x * image.cols + y;
+				if (image.data[k] == 255) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
 
-    //if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-    //  cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
+	void imageCb(const sensor_msgs::ImageConstPtr& msg) {
+		cv_bridge::CvImagePtr cv_ptr;
+		try {
+			cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+		} catch (cv_bridge::Exception& e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
 
 // Bluring and thresholding
-    cv::GaussianBlur(cv_ptr->image,cv_ptr->image,cv::Size(5,5),5,1);
-//    cv::Mat frame = findRed(cv_ptr->image); //find red
-    cv::Mat frame = findYellow(cv_ptr->image); //find yellow
+		cv::GaussianBlur(cv_ptr->image, cv_ptr->image, cv::Size(5, 5), 5, 1);
+		cv::Mat frame1 = findRed(cv_ptr->image); //find red
+		cv::Mat frame2 = findYellow(cv_ptr->image); //find yellow
 // Show original and processed image
-    cv::medianBlur(frame, frame, 5);
+		cv::medianBlur(frame1, frame1, 5);
+		cv::medianBlur(frame2, frame2, 5);
 
-int count = 0;
+		int redCount = count(frame1);
+		int yellowCount = count(frame2);
 
-    for(int x=0; x<frame.rows; x++){
-	for(int y=0; y<frame.cols; y++){
-	    int k = x*frame.cols + y;
-	    if(frame.data[k] == 255){
-		count++;
-	    }
+		printf("White pixels: %d\n", std::max(redCount, yellowCount));
+
+		cv::Mat frame = frame1;
+
+		if(redCount > 6000){
+			printf("Tomato found!\n");
+		}else if(yellowCount > 2000){
+			printf("Lemon found!\n");
+			frame = frame2;
+		}
+
+		cv::imshow(WINDOW2, frame);
+		cv::imshow(WINDOW, cv_ptr->image);
+		cv::waitKey(3);
+
+		image_pub_.publish(cv_ptr->toImageMsg());
 	}
-    } 
-
-    printf("White pixels: %d\n", count); 
-
-    std::vector<std::vector<cv::Point> > contours;
-    cv::Mat contourOutput = frame.clone();
-    cv::findContours( contourOutput, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE );
-
-    /*try {
-    printf("now im trying\n");
-    double area = fabs(contourArea(contours));
-    /*printf("Calculated the area\n");
-    if (area>10.0){
-	string text = "Object";
-	int fontFace = FONT_HERSHEY_SCRIPT_SIMPLE;
-	double fontScale = 2;
-	int thickness = 3;
-	cv::putText(cv_ptr->image, "object", textOrg, fontFace, fontScale, Scalar::all(255), thickness,8);
-	cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0))};*/
-    /*}
-    catch (cv::Exception){
-    }*/
-
-    cv::imshow(WINDOW2, frame);
-    cv::imshow(WINDOW,cv_ptr->image);
-    cv::waitKey(3);
-    
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
 };
 
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "image_converter");
+	ImageConverter ic;
+	ros::spin();
+	return 0;
 }
 
--- tutorialROSOpenCV/src/meta.cpp
+++ tutorialROSOpenCV/src/meta.cpp
@@ -4,61 +4,113 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <cmath>
 
 namespace enc = sensor_msgs::image_encodings;
 
 static const char WINDOW[] = "Image window";
 
-class ImageConverter
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
-  
+class Color {
 public:
-  ImageConverter()
-    : it_(nh_)
-  {
-    image_pub_ = it_.advertise("out", 1);
-    image_sub_ = it_.subscribe("/camera/rgb/image_color", 1, &ImageConverter::imageCb, this);
-
-    cv::namedWindow(WINDOW);
-  }
-
-  ~ImageConverter()
-  {
-    cv::destroyWindow(WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
-    cv::imshow(WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-    
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
+	Color(int r, int g, int b) {
+		c[0] = r;
+		c[1] = g;
+		c[2] = b;
+	}
+	double c[3];
 };
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+class ImageConverter {
+private:
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;
+
+public:
+	ImageConverter() :
+			it_(nh_) {
+		image_pub_ = it_.advertise("out", 1);
+		image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
+				&ImageConverter::imageCb, this);
+
+		cv::namedWindow(WINDOW);
+
+	}
+
+	~ImageConverter() {
+		cv::destroyWindow(WINDOW);
+	}
+
+	void imageCb(const sensor_msgs::ImageConstPtr& msg) {
+
+		Color tomato(120, 10, 0);
+		Color lemon(200, 175, 0);
+
+		cv_bridge::CvImagePtr cv_ptr;
+		try {
+			cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+		} catch (cv_bridge::Exception& e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+
+		int kk = 430 * 640 + 350;
+		int rr = cv_ptr->image.data[3 * kk + 2];
+		int gg = cv_ptr->image.data[3 * kk + 1];
+		int bb = cv_ptr->image.data[3 * kk];
+		cv::circle(cv_ptr->image, cv::Point(350, 430), 10, CV_RGB(rr,gg,bb));
+		printf("Circle color: %d, %d, %d\n", rr, gg, bb);
+
+		double minDist = 3 * 255 * 255;
+		int pointX, pointY;
+		int rgbSaved[3];
+		for (int y = 0; y < cv_ptr->image.rows; y++) {
+			for (int x = 0; x < cv_ptr->image.cols; x++) {
+				int k = x + y * cv_ptr->image.cols;
+				char b = cv_ptr->image.data[3 * k];
+				char g = cv_ptr->image.data[3 * k + 1];
+				char r = cv_ptr->image.data[3 * k + 2];
+				//double dist = colorDistance(b, g, r, 0, 10, 110);
+				double tomatoDist = colorDistance(Color(r, g, b), tomato);
+				double lemonDist = colorDistance(Color(r, g, b), lemon);
+				if (tomatoDist < 20) {
+					cv_ptr->image.data[3 * k] = 0;
+					cv_ptr->image.data[3 * k + 1] = 0;
+					cv_ptr->image.data[3 * k + 2] = 255;
+				}
+				if (lemonDist < 20) {
+					cv_ptr->image.data[3 * k] = 0;
+					cv_ptr->image.data[3 * k + 1] = 255;
+					cv_ptr->image.data[3 * k + 2] = 255;
+				}
+
+			}
+		}
+		cv::imshow(WINDOW, cv_ptr->image);
+		cv::waitKey(3);
+
+		image_pub_.publish(cv_ptr->toImageMsg());
+	}
+
+	double colorDistance(Color col1, Color col2) {
+		return sqrt(
+				pow(col1.c[0] - col2.c[0], 2) + pow(col1.c[1] - col2.c[1], 2)
+						+ pow(col1.c[2] - col2.c[2], 2));
+	}
+
+	double colorDistance(int b1, int g1, int r1, int b2, int g2, int r2) {
+		double dist = sqrt(
+				(r1 - r2) * (r1 - r2) + (g1 - g2) * (g1 - g2)
+						+ (b1 - b2) * (b1 - b2));
+		return dist;
+	}
+};
+
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "image_converter");
+	ImageConverter ic;
+	ros::spin();
+	return 0;
 }
 
