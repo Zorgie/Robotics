f1905965a50df7ca93db1f62b49d8ba238bc56f3
robo <robo@robo-VirtualBox.(none)>
2013-11-12 10:08:24 +0100

    Use the main.cpp to detect yellow and red. Right now one has to choose which color to detect in the code. The parameter values on cv::Scalar need adjustment

--- tutorialROSOpenCV/src/test.cpp
+++ tutorialROSOpenCV/src/test.cpp
@@ -2,17 +2,22 @@
 #include <highgui.h> /* required to use OpenCV's highgui */
 #include <stdio.h>
  
+IplImage* GetThresholdedImage(IplImage* imgHSV){       
+       IplImage* imgThresh=cvCreateImage(cvGetSize(imgHSV),IPL_DEPTH_8U, 1);
+       cvInRangeS(imgHSV, cvScalar(0,0,0), cvScalar(50,50,250), imgThresh);
+       return imgThresh;
+} 
+ 
+
+
 int main(int argc, char *argv[]) {
  IplImage* img=0; /* pointer to an image */
  printf("Hello\n");
  img = cvLoadImage("tomato.jpeg", 1);
- //if(argv[1] != 0)
- // img = cvLoadImage(argv[1], 1); // 1 for color
- //else
- // printf("File not found");
+ IplImage* frame = GetThresholdedImage(img);
  if(img != 0) {
   cvNamedWindow("Display", CV_WINDOW_AUTOSIZE); // create a window
-  cvShowImage("Display", img); // show image in window
+  cvShowImage("Display", frame); // show image in window
   cvWaitKey(0); // wait until user hits a key
   cvDestroyWindow("Display");
  }
