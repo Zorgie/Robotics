9d0e3ca00f4b275717ac28c485914abba5a8d460
robo <robo@robo-VirtualBox.(none)>
2013-11-14 13:53:44 +0100

    Line detection added

---
+++ tutorialROSOpenCV/src/ImageConverter.h
@@ -0,0 +1,40 @@
+/*
+ * derp.h
+ *
+ *  Created on: Nov 14, 2013
+ *      Author: robo
+ */
+
+#ifndef IMAGE_CONVERTER_H_
+#define IMAGE_CONVERTER_H_
+
+#include <ros/ros.h>
+#include <cv.h>
+#include <highgui.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+#include <cmath>
+
+namespace enc = sensor_msgs::image_encodings;
+
+class ImageConverter {
+
+public:
+	ImageConverter() {
+	}
+
+	~ImageConverter() {
+	}
+
+	int count(cv::Mat image);
+
+	cv::Mat getHoughLines(cv::Mat img);
+
+	cv_bridge::CvImagePtr getImage(const sensor_msgs::ImageConstPtr& msg);
+};
+
+#endif /* IMAGE_CONVERTER_H_ */
